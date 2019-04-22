e975457e9450afa77dec5002c90f1b4a3eeeacd0
Erick Ferreira <erickflopes1@gmail.com>
2019-04-21 22:53:25 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/erickferreira/cursomc/domain/Cliente.java
+++ src/main/java/com/erickferreira/cursomc/domain/Cliente.java
@@ -12,11 +12,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.erickferreira.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
@@ -40,6 +39,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
---
+++ src/main/java/com/erickferreira/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.erickferreira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.erickferreira.cursomc.domain.Pedido;
+import com.erickferreira.cursomc.repositories.PedidoRepository;
+import com.erickferreira.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
